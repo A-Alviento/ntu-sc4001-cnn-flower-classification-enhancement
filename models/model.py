14ea850fa2e8b68c762e32bd9dc9eb686fc9f758
mk43v3r <mk43v3r@gmail.com>
2023-11-04 02:33:15 +0000

    Depthwise + Pointwise model with data

--- models/model.py
+++ models/model.py
@@ -234,3 +234,73 @@ class BatchNormReducedCNN(SkeletonCNN):
         )
 
         self.calculate_pin_indexes()
+
+# Standard convolution layers replaced with depthwise + pointwise convolution layers
+class DepthPointWiseCNN(SkeletonCNN):
+    def __init__(self, num_classes=102, fcn_depth=3, fcn_width=[1024, 512], dropout_rate=0.5, batchnorm_moment=0.05):
+        super().__init__(fcn_depth, fcn_width)
+        
+        self.conv_stack = nn.Sequential(
+            ## Convolutional Layers
+            # takes an input with 3 channels, applies 64 kernels of size 5x5 and outputs 64 feature map
+            nn.BatchNorm2d(3, momentum=None),  # Use total running average
+            nn.Conv2d(3, 3, kernel_size=5, stride=1, padding=2, groups=3),
+            self.pin(nn.Conv2d(3, 64, kernel_size=1)),
+            nn.ReLU(),
+            self.pin(nn.LocalResponseNorm(5, 0.0001, 0.75, 2)),
+            self.pin(nn.MaxPool2d(3, stride=2)),
+        
+            # takes an input with 64 channels, applies 128 kernels of size 5x5 and outputs 128 feature map
+            nn.BatchNorm2d(64, momentum=batchnorm_moment),
+            nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=2, groups=64),
+            self.pin(nn.Conv2d(64, 128, kernel_size=1)),
+            nn.ReLU(),
+            self.pin(nn.LocalResponseNorm(5, 0.0001, 0.75, 2)),
+            self.pin(nn.MaxPool2d(3, stride=2)),
+            
+            # takes an input with 128 channels, applies 256 kernels of size 3x3 and outputs 256 feature map
+            nn.BatchNorm2d(128, momentum=batchnorm_moment),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, groups=128),
+            self.pin(nn.Conv2d(128, 256, kernel_size=1)),
+            nn.ReLU(),
+            self.pin(nn.MaxPool2d(3, stride=2)),
+        
+            # takes an input with 256 channels, applies 512 kernels of size 3x3 and outputs 512 feature map
+            # paper details to skip the pooling layer after the fourth convolutional layer
+            nn.BatchNorm2d(256, momentum=batchnorm_moment),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, groups=256),
+            self.pin(nn.Conv2d(256, 512, kernel_size=1)),
+            nn.ReLU(),
+        
+            # takes an input with 512 channels, applies 512 kernels of size 3x3 and outputs 512 feature map
+            nn.BatchNorm2d(512, momentum=batchnorm_moment),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, groups=512),
+            self.pin(nn.Conv2d(512, 512, kernel_size=1)),
+            nn.ReLU(),
+            self.pin(nn.MaxPool2d(3, stride=2)),    
+        )
+
+        # Initialise the fully connected layers in a list, so that we can unpack it in nn.Sequential
+        fcn_list = []
+        input_size = 5 * 5 * 512
+        for width in fcn_width:
+            fcn_list.extend([
+                nn.Linear(input_size, width),
+                nn.ReLU(),
+                nn.Dropout(dropout_rate),
+            ])
+            input_size = width
+
+        self.fcn_stack = nn.Sequential(
+            # flatten the output of the last convolutional layer
+            nn.Flatten(),
+
+            # Fully-connected layers unpacked
+            *fcn_list,
+            
+            # output layer
+            nn.BatchNorm1d(input_size, momentum=batchnorm_moment),
+            self.pin(nn.Linear(input_size, num_classes)),
+        )
+
+        self.calculate_pin_indexes()
